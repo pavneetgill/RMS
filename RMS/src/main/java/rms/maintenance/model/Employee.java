2911c3b1b3a36980ba483c33e434f9d1b5bee3c4
Pavneet Gill <pavneetgill@gmail.com>
2017-11-04 23:10:39 -0400

    Employee Table

--- RMS/src/main/java/rms/maintenance/model/Employee.java
+++ RMS/src/main/java/rms/maintenance/model/Employee.java
@@ -1,9 +1,11 @@
 package rms.maintenance.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
@@ -11,40 +13,54 @@ import lombok.Data;
 
 
 @Entity
-
+@Table(name="employee")
 public @Data class Employee {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-
+	@Column(name="empId")
 	private int empId;
 
+	@Column(name="deptId")
 	private int deptId;
 
+	@Column(name="empType")
 	private int empType;
 
+	@Column(name="fname")
 	private String fname;
 
+	@Column(name="lname")
 	private String lname;
 
-	private String mInitial;
+	@Column(name="minitial")
+	private String minitial;
 
+	@Column(name="dob")
 	private String dob;
 
+	@Column(name="gender")
 	private String gender;
 
+	@Column(name="address1")
 	private String address1;
 
-	private String addresss2;
+	@Column(name="address2")
+	private String address2;
 
+	@Column(name="city")
 	private String city;
 
+	@Column(name="state")
 	private String state;
 
+	@Column(name="email")
 	private String email;
 
+	@Column(name="phoneNumber")
 	private String phoneNumber;
 
+	@Column(name="managerId")
 	private String managerId;
 
 
